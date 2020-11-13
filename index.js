4d904daaad1140f60d54009210b43a971c09c7fd
tranvan110 <tranvan456@gmail.com>
2020-11-14 03:02:31 +0700

    initial commit

--- index.js
+++ index.js
@@ -32,31 +32,41 @@ webApp.get('/', (req, res) => res.render('index', { value: null }));
 var piws;
 
 wss.on('connection', (ws) => {
-	console.log('Client connected');
+	console.log(`Client connected`);
 	ws.on('close', () => {
 		console.log('Client disconnected');
 	});
 
-	ws.on('message', function (data, flags) {
-		if (flags) { return; }
-		console.log(data);
-		if (data == "pi_online")
-			piws = ws;
-		Dialogflow(data, function (error, msg) {
-			if (error) {
-				ws.send('Error');
-			}
-			else {
-				console.log(msg);
-				ws.send(msg);
-			}
-		});
+	ws.on('error', function () {
+		console.log('Connection error');
+	});
 
-		ws.on('close', function () {
-			console.log('Connection closed!');
-		});
-		ws.on('error', function (e) {
-		});
+	ws.on('message', function (data, flags) {
+		console.log(data);
+		if (flags) {
+			return;
+		}
+		if (data == "pi_online") {
+			piws = ws;
+		}
+		else if (ws == piws) {
+			wss.clients.forEach(function each(client) {
+				if (client !== ws && client.readyState === WebSocket.OPEN) {
+					client.send(data);
+				}
+			});
+		}
+		else {
+			Dialogflow(data, function (error, msg) {
+				console.log(msg);
+				if (error) {
+					ws.send('Error');
+				}
+				else {
+					ws.send(msg);
+				}
+			});
+		}
 	});
 });
 
@@ -76,44 +86,44 @@ async function Dialogflow(msg, callback) {
 	const result = responses[0].queryResult;
 	const parameters = result.parameters.fields;
 
+	console.log('parameters: ');
 	console.log(parameters);
 
 	if (result.intent) {
-		console.log(`  Intent: ${result.intent.displayName}`);
-		console.log(`  Query: ${result.queryText}`);
-		console.log(`  Response: ${result.fulfillmentText}`);
+		console.log(`\nIntent: ${result.intent.displayName}`);
+		console.log(`Query: ${result.queryText}`);
+		console.log(`Response: ${result.fulfillmentText}`);
 	} else {
-		console.log(`  No intent matched.`);
+		console.log('No intent matched.');
 	}
 
 	if (result.action == 'input.thoitiet') {
-		if (parameters.City.stringValue == Nha) {
+		if (parameters.City.stringValue == 'Nha' && piws.readyState === WebSocket.OPEN) {
 			console.log("Command: tem?");
 			piws.send("tem?");
 		}
 		else {
 			var city = result.parameters.fields.City.stringValue;
 			var url = encodeURI(`${weather}&query=${city},Vietnam`);
-			console.log(url);
 			request(url, function (error, response, body) {
-				if (error)
+				if (error) {
 					return callback(error);
-				else {
-					var weather = JSON.parse(body).current;
-					var msg = `Trạng thái: ${weather.weather_descriptions}, tầm nhìn: ${weather.visibility} Km\n`;
-					msg += `Nhiệt độ: ${weather.temperature} độ, cảm giác như: ${weather.feelslike} độ\n`;
-					msg += `Độ ẩm: ${weather.humidity}%, áp suất không khí: ${weather.pressure}\n`;
-					msg += `Tốc độ gió: ${weather.wind_speed}, chỉ số UV: ${weather.uv_index}\n`;
-					return callback(null, msg);
 				}
+				var weather = JSON.parse(body).current;
+				var msg = `Trạng thái: ${weather.weather_descriptions}, tầm nhìn: ${weather.visibility} Km\n`;
+				msg += `Nhiệt độ: ${weather.temperature} độ, cảm giác như: ${weather.feelslike} độ\n`;
+				msg += `Độ ẩm: ${weather.humidity}%, áp suất không khí: ${weather.pressure}\n`;
+				msg += `Tốc độ gió: ${weather.wind_speed}, chỉ số UV: ${weather.uv_index}\n`;
+				return callback(null, msg);
 			});
 		}
 	}
 	else {
 		var msg = parameters.action.stringValue + parameters.device.stringValue;
 		console.log(`Command: ${msg}`);
-		if (piws)
+		if (piws.readyState === WebSocket.OPEN) {
 			piws.send(msg);
+		}
 		return callback(null, result.fulfillmentText);
 	}
 
